27cd352ab5e7d2433796b7c795ea3232d859faac
FabianGremme <fagre101@hhu.de>
2024-12-10 14:39:01 +0000

    added structs for cxl.mem messages

--- os/kernel/src/memory/messages.rs
+++ os/kernel/src/memory/messages.rs
@@ -81,3 +81,70 @@ bitfield!{
     go_err,_:15, 15;
     rsvd,_:23, 16;
 }
+
+
+// das hier sind nun die Nachrichten vom CXL.mem Protokoll:
+
+bitfield!{
+    struct m2s_request(MSB0[usize]);
+    u16;
+    valid,_:0, 0;
+    mem_opcode,_:4, 1;
+    meta_field,_:6, 5;
+    meta_value,_:8, 7;
+    snp_type,_:11, 9;
+    address,_:58, 12;
+    tag,_:74, 59;
+    tc,_:76, 75;
+    rsvd,_:86, 77;
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum M2S_Req_Oppcode {
+    MemInv = 1,
+    MemRd = 2,
+    MemRdData = 3,
+    MemRdFwd = 4,
+    MemWrFwd = 5,
+    MemInvNT = 6,
+    Reserved = 7,
+}
+
+bitfield!{
+    struct m2s_request_with_data(MSB0[usize]);
+    u16;
+    valid,_:0, 0;
+    mem_opcode,_:4, 1;
+    meta_field,_:6, 5;
+    meta_value,_:8, 7;
+    snp_type,_:11, 9;
+    address,_:57, 12;
+    tag,_:73, 58;
+    tc,_:75, 74;
+    poison,_:76, 76;
+    rsvd,_:86, 77;
+}
+
+bitfield!{
+    struct s2m_no_data_response(MSB0[usize]);
+    u16;
+    valid,_:0, 0;
+    opcode,_:3, 1;
+    meta_field,_:5, 4;
+    meta_value,_:7, 6;
+    tag,_:23, 8;
+    rsvd,_:27, 24;
+}
+
+bitfield!{
+    struct s2m_data_response(MSB0[usize]);
+    u16;
+    valid,_:0, 0;
+    opcode,_:3, 1;
+    meta_field,_:5, 4;
+    meta_value,_:7, 6;
+    tag,_:23, 8;
+    poison,_:24, 24;
+    rsvd,_:39, 25;
+}
